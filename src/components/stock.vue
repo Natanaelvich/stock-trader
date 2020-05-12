78075e5f771e656f30b41a71e0cadda302e912c2
Natanael Lima <taelima1997@gmail.com>
2020-05-12 00:07:22 -0300

    created stock card and stock list

--- src/components/stock.vue
+++ src/components/stock.vue
@@ -1,9 +1,52 @@
 <template>
-  <h1>Stock Item</h1>
+  <div class="box">
+    <div class="title">
+      <strong>Nome <small>(Preço : PRECO)</small></strong>
+    </div>
+    <div class="form">
+      <form>
+        <input type="text" />
+        <button>COMPRAR</button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+.box {
+  background: #ddd;
+  margin: 10px;
+}
+.box .title {
+  background: green;
+  padding: 10px 5px;
+  color: #fff;
+}
+
+.box .form {
+  padding: 20px 0;
+}
+.box .form form {
+  display: flex;
+  justify-content: center;
+  flex: 1;
+  padding: 0 5px;
+}
+.form form input {
+  flex: 1;
+  border: 0;
+  border-bottom: 2px solid #fff;
+  background: transparent;
+}
+.form form button {
+  border: 0;
+  background: green;
+  color: #fff;
+  padding: 3px 8px;
+  text-align: center;
+}
+</style>
--- src/pages/stockList.vue
+++ src/pages/stockList.vue
@@ -1,17 +1,51 @@
 <template>
   <div class="stock-list">
-    <h1>StokList</h1>
+    <dic class="container">
+      <div class="cards">
+        <Stock v-for="stock in stocks" :key="stock.id" />
+      </div>
+    </dic>
   </div>
 </template>
 
 <script>
-export default {};
+import Stock from "../components/stock";
+export default {
+  components: {
+    Stock,
+  },
+
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: "BMW", price: 110 },
+        { id: 2, name: "Google", price: 200 },
+        { id: 3, name: "Apple", price: 330 },
+        { id: 4, name: "Twitter", price: 440 },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
+<style scoped>
 .stock-list {
   animation-name: slide-right;
   animation-duration: 0.5s;
+}
+
+.cards {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+  margin-bottom: 30px;
+  margin-top: 40px;
+  padding: 0 40px;
+}
+@media (max-width: 383px) {
+  .cards {
+    grid-template-columns: repeat(1, 1fr);
+  }
 }
 
 /* animation slide right */
